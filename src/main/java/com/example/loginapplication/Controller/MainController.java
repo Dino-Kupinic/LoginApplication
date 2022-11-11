89e98b47981e2f72e61c2e9b155702cc959cf890
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2022-11-11 21:03:24 +0100

    added Player class added Player sprite

--- src/main/java/com/example/loginapplication/Controller/MainController.java
+++ src/main/java/com/example/loginapplication/Controller/MainController.java
@@ -1,4 +1,16 @@
 package com.example.loginapplication.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class MainController {
+    @FXML
+    private Button settingsButton;
+    @FXML
+    private Button infoButton;
+    @FXML
+    private Label welcomeLabel;
+
+
 }
---
+++ src/main/java/com/example/loginapplication/Modell/Player.java
@@ -0,0 +1,31 @@
+package com.example.loginapplication.Modell;
+
+public class Player {
+    private int health;
+    private int energy;
+    private int level;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+}
