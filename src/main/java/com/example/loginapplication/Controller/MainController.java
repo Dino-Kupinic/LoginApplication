fac8eea3c58c9597825a54a378a7ef804e10332c
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2022-11-12 14:46:09 +0100

    trying to change backgroundcolor based on colorpicker

--- src/main/java/com/example/loginapplication/Controller/MainController.java
+++ src/main/java/com/example/loginapplication/Controller/MainController.java
@@ -1,12 +1,18 @@
 package com.example.loginapplication.Controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
     @FXML
@@ -15,13 +21,27 @@ public class MainController {
     private Button infoButton;
     @FXML
     private Label welcomeLabel;
+    @FXML
+    private static AnchorPane mainBackground;
 
     @FXML
-    private void onSettingsButtonClick() {
+    public static void changeBackgroundColor(Color bgcolor) {
+        String color = String.valueOf(bgcolor);
+        System.out.println(color);
+        mainBackground.setStyle("-fx-background-color: " + color);
+    }
+
+    @FXML
+    private void onSettingsButtonClick() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Settings-view.fxml"));
+        Parent root = fxmlLoader.load();
         Stage settingsStage = new Stage();
-        Scene settingsScene = new Scene(new Group(), 400, 500);
+        Scene settingsScene = new Scene(root);
+
+
         settingsStage.setTitle("Settings");
         settingsStage.initModality(Modality.APPLICATION_MODAL);
+        settingsStage.setResizable(false);
         settingsStage.setScene(settingsScene);
         settingsStage.show();
 
--- src/main/java/com/example/loginapplication/Controller/SettingsController.java
+++ src/main/java/com/example/loginapplication/Controller/SettingsController.java
@@ -1,4 +1,17 @@
 package com.example.loginapplication.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.paint.Color;
+
 public class SettingsController {
+    @FXML
+    private ColorPicker colorPicker;
+
+    @FXML
+    private void onColorPickerAdjust() {
+        Color bgColor = colorPicker.getValue();
+        MainController.changeBackgroundColor(bgColor);
+        System.out.println(bgColor);
+    }
 }
