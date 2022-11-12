7dbb270125802f9e68f4c5465ed6c456710204e5
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2022-11-12 12:24:41 +0100

    added new files

---
+++ src/main/java/com/example/loginapplication/Controller/SettingsController.java
@@ -0,0 +1,4 @@
+package com.example.loginapplication.Controller;
+
+public class SettingsController {
+}
---
+++ src/main/java/com/example/loginapplication/Modell/Move.java
@@ -0,0 +1,5 @@
+package com.example.loginapplication.Modell;
+
+public enum Move {
+    UP, DOWN, LEFT, RIGHT
+}
